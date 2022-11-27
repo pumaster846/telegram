7a0985a285f4927418e8ff92b2559dc69698d4a9
pumaster846 <112078517+pumaster846@users.noreply.github.com>
2022-11-27 18:42:14 +0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -6,7 +6,34 @@ const API_TOKEN = "5888375092:AAGYWV58LLmmDQnvaZv_litXbTnqIg6h1ZE";
 
 $jsonData = json_decode(file_get_contents('php://input'), true);
 
-var_dump($jsonData);
+$message = mb_strtolower($jsonData['message']['text'], 'utf-8');
+
+function sendRequest(string $method, array $options = []) {
+    $initializer = curl_init();
+
+    $url = API_URL . API_TOKEN . '/' . $method;
+    //https://api.telegram.org/bot5888375092:AAGYWV58LLmmDQnvaZv_litXbTnqIg6h1ZE/getUpdates?offset=607341323
+
+    if(!empty($options)) {
+        $url .= '?' . http_build_query($options);
+    }
+
+    $setoptsArray = array(
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_CONNECTTIMEOUT => 10
+    );
+
+    curl_setopt_array($initializer, $setoptsArray);
+
+    $response = curl_exec($initializer);
+
+    curl_close($initializer);
+
+    return json_decode($response, true);
+}
+
+sendRequest('sendMessage', ['text' => $message]);
 
 ?>
 </pre>
