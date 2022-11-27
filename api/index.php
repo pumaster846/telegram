5c4b21d50dd9925c7c0440ef0ac4cbb016218bed
pumaster846 <112078517+pumaster846@users.noreply.github.com>
2022-11-27 15:53:49 +0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,27 +1,39 @@
 <pre>
-
 <?php
-var_dump(opcache_get_status());
 
 const API_URL = "https://api.telegram.org/bot";
 const API_TOKEN = "5888375092:AAGYWV58LLmmDQnvaZv_litXbTnqIg6h1ZE";
 
-$update = json_decode(file_get_contents('php://input'), true);
+$jsonData = json_decode(file_get_contents('php://input'), true);
+
+print_r($jsonData);
 
 function sendRequest(string $method, array $options = []) {
-    $requestURL = API_URL . API_TOKEN . '/' . $method;
+    $initializer = curl_init();
+
+    $url = API_URL . API_TOKEN . '/' . $method;
+    //https://api.telegram.org/bot5888375092:AAGYWV58LLmmDQnvaZv_litXbTnqIg6h1ZE/getUpdates?offset=607341323
 
     if(!empty($options)) {
-        $requestURL .= '?' . http_build_query($options);
+        $url .= '?' . http_build_query($options);
     }
-    // Декодинг полученных данных
-    return json_decode(file_get_contents($requestURL), true);
+
+    $setoptsArray = array(
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_CONNECTTIMEOUT => 10
+    );
+
+    curl_setopt_array($initializer, $setoptsArray);
+
+    $response = curl_exec($initializer);
+
+    curl_close($initializer);
+
+    return json_decode($response, true);
 }
 
-$chat_id = $update['message']['chat']['id'];
+$chat_id = $jsonData['message']['chat']['id'];
 sendRequest('sendMessage', ['chat_id' => $chat_id, 'text' => 'Privet']);
 
-var_dump($update);
-?>
-
-</pre>
+echo "<br><br>" . $chat_id;
