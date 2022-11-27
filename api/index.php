ea269728ccf4cf19292c6288070de88c8c7a7cff
pumaster846 <112078517+pumaster846@users.noreply.github.com>
2022-11-27 11:19:00 +0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,3 +1,10 @@
 <?php
 phpinfo();
 var_dump(opcache_get_status());
+
+const API_URL = "https://api.telegram.org/bot";
+const API_TOKEN = "5888375092:AAGYWV58LLmmDQnvaZv_litXbTnqIg6h1ZE";
+
+$update = json_decode(file_get_contents('php://input'), true);
+
+var_dump($update);
