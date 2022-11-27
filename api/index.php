4c926c88498014117e941e3839429304ea8cb3b3
pumaster846 <112078517+pumaster846@users.noreply.github.com>
2022-11-27 16:17:49 +0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,43 +1,101 @@
 <pre>
 <?php
 
-const API_URL = "https://api.telegram.org/bot";
-const API_TOKEN = "5888375092:AAGYWV58LLmmDQnvaZv_litXbTnqIg6h1ZE";
-
-$jsonData = json_decode(file_get_contents('php://input'), true);
-
-var_dump(json_decode(file_get_contents('php://input'), true));
+# Принимаем запрос
+$data = json_decode(file_get_contents('php://input'), TRUE);
+//file_put_contents('file.txt', '$data: '.print_r($data, 1)."\n", FILE_APPEND);
 
 
-function sendRequest(string $method, array $options = []) {
-    $initializer = curl_init();
+//https://api.telegram.org/bot*Токен бота*/setwebhook?url=*ссылка на бота*
 
-    $url = API_URL . API_TOKEN . '/' . $method;
-    //https://api.telegram.org/bot5888375092:AAGYWV58LLmmDQnvaZv_litXbTnqIg6h1ZE/getUpdates?offset=607341323
 
-    if(!empty($options)) {
-        $url .= '?' . http_build_query($options);
-    }
+# Обрабатываем ручной ввод или нажатие на кнопку
+$data = $data['callback_query'] ? $data['callback_query'] : $data['message'];
 
-    $setoptsArray = array(
-        CURLOPT_URL => $url,
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_CONNECTTIMEOUT => 10
-    );
+# Важные константы
+define('TOKEN', '5888375092:AAGYWV58LLmmDQnvaZv_litXbTnqIg6h1ZE');
 
-    curl_setopt_array($initializer, $setoptsArray);
+# Записываем сообщение пользователя
+$message = mb_strtolower(($data['text'] ? $data['text'] : $data['data']),'utf-8');
 
-    $response = curl_exec($initializer);
 
-    curl_close($initializer);
+# Обрабатываем сообщение
+switch ($message)
+{
+    case 'текст':
+        $method = 'sendMessage';
+        $send_data = [
+            'text'   => 'Вот мой ответ'
+        ];
+        break;
 
-    return json_decode($response, true);
+    case 'кнопки':
+        $method = 'sendMessage';
+        $send_data = [
+            'text'   => 'Вот мои кнопки',
+            'reply_markup' => [
+                'resize_keyboard' => true,
+                'keyboard' => [
+                    [
+                        ['text' => 'Видео'],
+                        ['text' => 'Кнопка 2'],
+                    ],
+                    [
+                        ['text' => 'Кнопка 3'],
+                        ['text' => 'Кнопка 4'],
+                    ]
+                ]
+            ]
+        ];
+        break;
+
+
+    case 'видео':
+        $method = 'sendVideo';
+        $send_data = [
+            'video'   => 'https://chastoedov.ru/video/amo.mp4',
+            'caption' => 'Вот мое видео',
+            'reply_markup' => [
+                'resize_keyboard' => true,
+                'keyboard' => [
+                    [
+                        ['text' => 'Кнопка 1'],
+                        ['text' => 'Кнопка 2'],
+                    ],
+                    [
+                        ['text' => 'Кнопка 3'],
+                        ['text' => 'Кнопка 4'],
+                    ]
+                ]
+            ]
+        ];
+        break;
+
+    default:
+        $method = 'sendMessage';
+        $send_data = [
+            'text' => 'Не понимаю о чем вы :('
+        ];
 }
 
+# Добавляем данные пользователя
+$send_data['chat_id'] = $data['chat']['id'];
 
-$chat_id = $jsonData['message']['chat']['id'];
-var_dump($jsonData['message']['chat']['id']);
-var_dump(sendRequest('sendMessage', ['chat_id' => $chat_id, 'text' => 'Privet']));
-//sendRequest('sendMessage', ['chat_id' => $chat_id, 'text' => 'Privet']);
+$res = sendTelegram($method, $send_data);
 
-echo "<br><br>" . $chat_id;
+function sendTelegram($method, $data, $headers = [])
+{
+    $curl = curl_init();
+    curl_setopt_array($curl, [
+        CURLOPT_POST => 1,
+        CURLOPT_HEADER => 0,
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_URL => 'https://api.telegram.org/bot' . TOKEN . '/' . $method,
+        CURLOPT_POSTFIELDS => json_encode($data),
+        CURLOPT_HTTPHEADER => array_merge(array("Content-Type: application/json"), $headers)
+    ]);   
+    
+    $result = curl_exec($curl);
+    curl_close($curl);
+    return (json_decode($result, 1) ? json_decode($result, 1) : $result);
+}
