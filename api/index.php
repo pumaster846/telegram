8784294fdf90966835a3e3a3f2edab3d07ab5e73
pumaster846 <112078517+pumaster846@users.noreply.github.com>
2022-11-27 11:11:52 +0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,15 +1,8 @@
 <?php
 phpinfo();
 
-
 const API_URL = "https://api.telegram.org/bot";
 const API_TOKEN = "5888375092:AAGYWV58LLmmDQnvaZv_litXbTnqIg6h1ZE";
-
-echo API_URL;
-print API_URL;
-var_dump(API_URL);
-
-$update = json_decode(file_get_contents('php://input'), true); //Прочитать POST запрос весь целиком
 
 function sendRequest(string $method, array $options = []) {
     $requestURL = API_URL . API_TOKEN . '/' . $method;
@@ -21,6 +14,8 @@ function sendRequest(string $method, array $options = []) {
     // Декодинг полученных данных
     return json_decode(file_get_contents($requestURL), true);
 }
+
+$update = json_decode(file_get_contents('php://input'), true);
 
 $chat_id = $update['message']['chat']['id'];
 sendRequest('sendMessage', ['chat_id' => $chat_id, 'text' => 'Privet'])
