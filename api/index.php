f798548cc2d2d8da861a7fcbf733157a40326637
pumaster846 <112078517+pumaster846@users.noreply.github.com>
2022-11-30 00:33:25 +0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -16,68 +16,106 @@ $releaseDate = '17.12.2002';
 
 switch ($message) {
     case '/start':
-        $method = 'sendMessage';
-        $methodOptions = [
-            'chat_id' => $chatId,
-            'parse_mode' => 'HTML',
-            'text' =>
-                "Привет, <b>{$userName}</b>!" . PHP_EOL .
-                "Я бот MirBellGet, моя версия {$version}, дата выпуска {$releaseDate}",
-            'reply_markup' => [
-                'resize_keyboard' => true,
-                'keyboard' => [
-                    [
-                        ['text' => 'Услуги']
-                    ],
-                    [
-                        ['text' => 'О нас'],
-                        ['text' => 'Контакты']
+        $request = [
+            [
+                $method = 'sendMessage';
+                $methodOptions = [
+                    'chat_id' => $chatId,
+                    'parse_mode' => 'HTML',
+                    'text' =>
+                        "Привет, <b>{$userName}</b>!" . PHP_EOL .
+                        "Я бот MirBellGet, моя версия {$version}, дата выпуска {$releaseDate}",
+                    'reply_markup' => [
+                        'resize_keyboard' => true,
+                        'keyboard' => [
+                            [
+                                ['text' => 'Услуги']
+                            ],
+                            [
+                                ['text' => 'О нас'],
+                                ['text' => 'Контакты']
+                            ]
+                        ]
                     ]
                 ]
+            ],
+            [
+                $method = 'sendMessage';
+                $methodOptions = [
+                    'chat_id' => $chatId,
+                    'parse_mode' => 'HTML',
+                    'text' =>
+                        "Привет, <b>{$userName}</b>!"
+                ]
             ]
-        ];
+        ]
     break;
 
-    case 'о нас':
-        $method = 'sendMessage';
-        $methodOptions = [
-            'chat_id' => $chatId,
-            'parse_mode' => 'HTML',
-            'text' =>
-                "<b>О компании</b>"
-                . PHP_EOL . "" . PHP_EOL .
-                "Информация о компании"
-        ];
-    break;
+    // case '/start':
+    //     $method = 'sendMessage';
+    //     $methodOptions = [
+    //         'chat_id' => $chatId,
+    //         'parse_mode' => 'HTML',
+    //         'text' =>
+    //             "Привет, <b>{$userName}</b>!" . PHP_EOL .
+    //             "Я бот MirBellGet, моя версия {$version}, дата выпуска {$releaseDate}",
+    //         'reply_markup' => [
+    //             'resize_keyboard' => true,
+    //             'keyboard' => [
+    //                 [
+    //                     ['text' => 'Услуги']
+    //                 ],
+    //                 [
+    //                     ['text' => 'О нас'],
+    //                     ['text' => 'Контакты']
+    //                 ]
+    //             ]
+    //         ]
+    //     ];
+    // break;
 
-    case 'контакты':
-        $method = 'sendContact';
-        $methodOptions = [
-            'chat_id' => $chatId,
-            'phone_number' => '8(900)999-99-99',
-            'first_name' => 'Name',
-            'last_name' => 'LastName'
-        ];
-    break;
+    // case 'о нас':
+    //     $method = 'sendMessage';
+    //     $methodOptions = [
+    //         'chat_id' => $chatId,
+    //         'parse_mode' => 'HTML',
+    //         'text' =>
+    //             "<b>О компании</b>"
+    //             . PHP_EOL . "" . PHP_EOL .
+    //             "Информация о компании"
+    //     ];
+    // break;
 
-    default:
-        $method = 'sendMessage';
-        $methodOptions = [
-            'chat_id' => $chatId,
-            'parse_mode' => 'HTML',
-            'text' =>
-                "<b>Наши контакты:</b>"
-                . PHP_EOL . "" . PHP_EOL .
-                "Номер телефона: {$phoneNumber}" . PHP_EOL .
-                "Почта: {$emailAdress}"
+    // case 'контакты':
+    //     $method = 'sendContact';
+    //     $methodOptions = [
+    //         'chat_id' => $chatId,
+    //         'phone_number' => '8(900)000-00-00',
+    //         'first_name' => 'Name',
+    //         'last_name' => 'LastName'
+    //     ];
+    // break;
 
-        ];
-    break;
+    // default:
+    //     $method = 'sendMessage';
+    //     $methodOptions = [
+    //         'chat_id' => $chatId,
+    //         'parse_mode' => 'HTML',
+    //         'text' =>
+    //             "<b>Наши контакты:</b>"
+    //             . PHP_EOL . "" . PHP_EOL .
+    //             "Номер телефона: {$phoneNumber}" . PHP_EOL .
+    //             "Почта: {$emailAdress}"
+
+    //     ];
+    // break;
 }
 
-sendRequest($method, $methodOptions);
+foreach ($request as $ap) {
+    sendRequest($ap);
+}
 
-function sendRequest($method, $jsonData, $headers = []) {
+function sendRequest($method = [], $jsonData, $headers = []) {
     $initializer = curl_init();
     
     curl_setopt_array($initializer, [
