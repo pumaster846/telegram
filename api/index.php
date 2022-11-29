f1c233dc79c1a02a08fb384bb4c0b15558dd570c
pumaster846 <112078517+pumaster846@users.noreply.github.com>
2022-11-30 02:32:00 +0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -2,64 +2,53 @@
 const API_URL = "https://api.telegram.org/bot";
 const API_TOKEN = "5888375092:AAGYWV58LLmmDQnvaZv_litXbTnqIg6h1ZE";
 
-class Api {
-    protected int $chatId;
-    protected string $userName;
-    protected string $userMessage;
+$jsonData = json_decode(file_get_contents('php://input'), true);
+$data = $jsonData['callback_query'] ? $jsonData['callback_query'] : $jsonData['message'];
 
-    public function setTelegramData() {
-        $data = json_decode(file_get_contents('php://input'), true);
-        $data = $data['callback_query'] ? $data['callback_query'] : $data['message'];
+$chatId = $data['chat']['id'];
+$userName = $data['chat']['first_name'];
+$userMessage = mb_strtolower(($data['text'] ? $data['text'] : $data['data']),'utf-8');
 
-        $this->chatId = $data['chat']['id'];
-        $this->userName = $data['chat']['first_name'];
-        $this->userMessage = mb_strtolower(($data['text'] ? $data['text'] : $data['data']),'utf-8');
-    }
-}
-
-$api = new Api();
-$api->setTelegramData();
-
-switch ($api->userMessage) {
+switch ($userMessage) {
     case '/start':
         $method = 'sendMessage';
-        $methodOptions = [
-            'chat_id' => $api->chatId,
+        $methodOptions = array(
+            'chat_id' => $chatId,
             'parse_mode' => 'HTML',
             'text' =>
-                "Привет, <b>{$api->userName}</b>!" . PHP_EOL .
+                "Привет, <b>{$userName}</b>!" . PHP_EOL .
                 "Я бот <b>MirBellGet</b>." . PHP_EOL .
-                "Моя версия:" . PHP_EOL .
-                "Дата выпуска:",
-            'reply_markup' => [
-                'resize_keyboard' => true,
-                'keyboard' => [
-                    [
-                        ['text' => 'Услуги']
-                    ],
-                    [
-                        ['text' => 'О нас'],
-                        ['text' => 'Контакты']
-                    ]
+                "Моя версия: {$version}" . PHP_EOL .
+                "Дата выпуска: {$releaseDate}",
+            'reply_markup' => array(
+                [
+                    ['text' => 'Услуги']
+                ],
+                [
+                    ['text' => 'О нас'],
+                    ['text' => 'Контакты']
                 ]
-            ]
-        ];
-    break;
+            )
+        );
 
     default:
         $method = 'sendMessage';
         $methodOptions = [
-            'chat_id' => $api->chatId,
+            'chat_id' => $chatId,
             'parse_mode' => 'HTML',
             'text' =>
-                "<b>Не знаю</b>"
+                "<b>Наши контакты:</b>"
+                . PHP_EOL . "" . PHP_EOL .
+                "Номер телефона: {$phoneNumber}" . PHP_EOL .
+                "Почта: {$emailAdress}"
+
         ];
     break;
 }
 
 sendRequest($method, $methodOptions);
 
-function sendRequest($method, $data, $headers = []) {
+function sendRequest($method, $jsonData, $headers = []) {
     $initializer = curl_init();
     
     curl_setopt_array($initializer, [
@@ -68,7 +57,7 @@ function sendRequest($method, $data, $headers = []) {
         CURLOPT_URL => API_URL . API_TOKEN . '/' . $method,
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_CONNECTTIMEOUT => 10,
-        CURLOPT_POSTFIELDS => json_encode($data),
+        CURLOPT_POSTFIELDS => json_encode($jsonData),
         CURLOPT_HTTPHEADER => array_merge(array("Content-Type: application/json"), $headers)
     ]);   
     
