de6388f80d9f339cf335f69fcc01428e9abf4812
pumaster846 <112078517+pumaster846@users.noreply.github.com>
2022-11-30 16:02:28 +0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,9 +1,15 @@
 <?php
+
 const API_URL = "https://api.telegram.org/bot";
 const API_TOKEN = "5888375092:AAGYWV58LLmmDQnvaZv_litXbTnqIg6h1ZE";
 
-$jsonData = json_decode(file_get_contents('php://input'), true);
-$data = $jsonData['callback_query'] ? $jsonData['callback_query'] : $jsonData['message'];
+function commandGetData() {
+    $jsonData = json_decode(file_get_contents('php://input'), true);
+    $data = $jsonData['callback_query'] ? $jsonData['callback_query'] : $jsonData['message'];
+    return $data;
+}
+
+$data = commandGetData();
 
 $userData = array(
     'chatId' => $data['chat']['id'],
