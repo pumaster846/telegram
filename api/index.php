ff73d695e9071326974559ca671388b58eb7043a
pumaster846 <112078517+pumaster846@users.noreply.github.com>
2022-12-01 19:41:32 +0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -6,7 +6,7 @@ class Bot {
     protected int $chat_id;
     protected string $user_name;
     protected string $user_message;
-    
+
     public function getChatId() {
         return $this->chat_id;
     }
@@ -16,23 +16,24 @@ class Bot {
     public function getUserMessage() {
         return $this->user_message;
     }
-    
+
     public function getJsonData() {
         $data = json_decode(file_get_contents('php://input'), true);
         return $data['callback_query'] ? $data['callback_query'] : $data['message'];
     }
 
-    public function commandSetData() {
-$data = self::getJsonData();
-        
-        $this->chat_id = $data['chat']['id'];
-        $this->user_name = $data['chat']['first_name'];
+    public function setJsonData() {
+        $data = self::getJsonData();
+
+        $this->chat_id      = $data['chat']['id'];
+        $this->user_name    = $data['from']['first_name'];
         $this->user_message = mb_strtolower(($data['text'] ? $data['text'] : $data['data']),'utf-8');
     }
-    public function commandSendRequest(string $method, array $methodOptions = []) {
+
+    public function sendRequest(string $method, array $methodOptions = []) {
         $initializer = curl_init();
         
-        curl_setopt_array($initializer, [
+        curl_setopt_array($initializer, array(
             CURLOPT_POST => true,
             CURLOPT_HEADER => false,
             CURLOPT_URL => API_URL . API_TOKEN . '/' . $method,
@@ -40,75 +41,71 @@ $data = self::getJsonData();
             CURLOPT_CONNECTTIMEOUT => 10,
             CURLOPT_POSTFIELDS => json_encode($methodOptions),
             CURLOPT_HTTPHEADER => array("Content-Type: application/json")
-        ]);
+        ));
         
-        $response = curl_exec($initializer);
+        curl_exec($initializer);
         curl_close($initializer);
-        
-        return (json_decode($response, 1) ? json_decode($response, 1) : $response);
+
+        /*
+            $response = curl_exec($initializer);
+            return (json_decode($response, 1) ? json_decode($response, 1) : $response);
+        */
     }
-    public function commandBuildRequest() {
-        
-    }
+
+    public function setWebhook() {}
 }
 
 $bot = new Bot();
-$bot->commandSetData();
+$bot->setJsonData();
 
 switch ($bot->getUserMessage()) {
-            case '/start':
-                $method = 'sendMessage';
-                $methodOptions = [
-                    'chat_id' => $bot->getChatId(),
-                    'parse_mode' => 'HTML',
-        
-                    'text' => "Привет, <b>{$bot->getUserName()}</b>! Я бот <b>MirBellGet</b>. Моя версия: {$version}. Дата выпуска: {$releaseDate}",
-        
-                    'reply_markup' => [
-                        'resize_keyboard' => true,
-                        'keyboard' => [
-                            [
-                                ['text' => 'Услуги']
-                            ],
-                            [
-                                ['text' => 'О нас'],
-                                ['text' => 'Контакты']
-                            ]
-                        ]
+    case '/start':
+        $methodOptions = array(
+            'chat_id' => $bot->getChatId(),
+            'parse_mode' => 'HTML',
+            'text' => "Добрый день, <b>{$bot->getUserName()}</b>!" . PHP_EOL . "Я бот <b>MirBellGet</b>",
+            'reply_markup' => [
+                'resize_keyboard' => true,
+                'keyboard' => [
+                    [
+                        ['text' => 'Услуги']
+                    ],
+                    [
+                        ['text' => 'О нас'],
+                        ['text' => 'Контакты']
                     ]
-                ];
-                $bot->commandSendRequest('sendMessage', ['chat_id' => $bot->getChatId(), 'text' => "Привет?"]);
-            break;
-        
-            case 'о нас':
-                $method = 'sendMessage';
-                $methodOptions = [
-                    'chat_id' => $bot->getChatId(),
-                    'parse_mode' => 'HTML',
-                    'text' =>
-                        "<b>О компании</b>"
-                        . PHP_EOL . "" . PHP_EOL .
-                        "Информация о компании"
-                ];
-            break;
-        
-            case 'контакты':
-                $method = 'sendContact';
-                $methodOptions = [
-                    'chat_id' => $bot->getChatId(),
-                    'phone_number' => '8(900)000-00-00',
-                    'first_name' => 'Имя',
-                    'last_name' => 'Фамилия'
-                ];
-            break;
-        
-            default:
-                $method = 'sendMessage';
-                $methodOptions = [
-                    'chat_id' => $bot->getChatId(),
-                    'parse_mode' => 'HTML',
-                    'text' => "Хз"
-                ];
-            break;
-        }
-        $bot->commandSendRequest($method, $methodOptions);
+                ]
+            ]
+        );
+        sendRequest('sendMessage', $methodOptions);
+        sendRequest('sendMessage', ['chat_id' => $bot->getChatId(), 'text' => "Смайл"]);
+    break;
+
+    case 'о нас':
+        $methodOptions = array(
+            'chat_id' => $bot->getChatId(),
+            'parse_mode' => 'HTML',
+            'text' => "<b>О компании</b>" . PHP_EOL . "" . PHP_EOL . "Информация о компании"
+        );
+        sendRequest('sendMessage', $methodOptions);
+    break;
+
+    case 'контакты':
+        $methodOptions = array(
+            'chat_id' => $bot->getChatId(),
+            'phone_number' => '8(900)000-00-00',
+            'first_name' => 'Имя',
+            'last_name' => 'Фамилия'
+        );
+        sendRequest('sendMessage', $methodOptions);
+    break;
+
+    default:
+        $methodOptions = array(
+            'chat_id' => $bot->getChatId(),
+            'parse_mode' => 'HTML',
+            'text' => "<b>{$bot->getUserName()}</b>, я не знаю такой команды"
+        );
+        sendRequest('sendMessage', $methodOptions);
+    break;
+}
