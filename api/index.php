efe1075435287ad1b9f1e97103ba0f3aa71648f7
pumaster846 <112078517+pumaster846@users.noreply.github.com>
2022-12-01 22:13:29 +0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -77,7 +77,7 @@ switch ($bot->getUserMessage()) {
             )
         );
         $bot->sendRequest('sendMessage', $methodOptions);
-        $bot->sendRequest('sendDice', ['chat_id' => $bot->getChatId(), 'emoji' => hex2bin('F09F91BE')]);
+        $bot->sendRequest('sendMessage', ['chat_id' => $bot->getChatId(), 'text' => hex2bin('F09FA496')]);
     break;
 
     case 'о нас':
